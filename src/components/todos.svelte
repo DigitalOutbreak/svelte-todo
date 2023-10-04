d118c8d487207fc2a1ab0e3f04eb9f20298ac37f
Whoppa <joeynalvarado@gmail.com>
2023-10-03 19:01:58 -0500

    edit todo

--- src/components/todos.svelte
+++ src/components/todos.svelte
@@ -2,12 +2,13 @@
   import type { ITodo } from '../types/todo';
   import AddTodo from './AddTodo.svelte';
   import Todo from './Todo.svelte';
+  import TodosLeft from './TodosLeft.svelte';
   // state
   let todos: ITodo[] = [
-    { id: '1e4a59703af84', text: 'Todo 1', completed: true },
-    { id: '9e09bcd7b9349', text: 'Todo 2', completed: false },
-    { id: '9e4273a51a37c', text: 'Todo 3', completed: false },
-    { id: '53ae48bf605cc', text: 'Todo 4', completed: false },
+    // { id: '1e4a59703af84', text: 'Todo 1', completed: true },
+    // { id: '9e09bcd7b9349', text: 'Todo 2', completed: false },
+    // { id: '9e4273a51a37c', text: 'Todo 3', completed: false },
+    // { id: '53ae48bf605cc', text: 'Todo 4', completed: false },
   ];
 
   // debug
@@ -15,7 +16,7 @@
 
   // computed
   $: todosAmount = todos.length;
-
+  $: incompleteTodos = todos.filter((todo) => !todo.completed).length;
   // methods
   function generateRandomId(): string {
     return Math.random().toString(16).slice(2);
@@ -32,13 +33,12 @@
 
   function toggleCompleted(event: MouseEvent): void {
     let { checked } = event.target as HTMLInputElement;
-
     todos = todos.map((todo) => ({ ...todo, completed: checked }));
   }
 
   function completeTodo(id: string): void {
     todos = todos.map((todo) => {
-      if (todo.id === id) {
+      if (todo.id !== id) {
         todo.completed = !todo.completed;
       }
       return todo;
@@ -48,6 +48,22 @@
   function removeTodo(id: string): void {
     todos = todos.filter((todo) => todo.id !== id);
   }
+
+  // Make a edit todo Method
+  // this needs to pass in an Id in order to know which todo to edit
+  // also needs to pass in a string or newTodo to know what to edit to
+
+  function editTodo(id: string, newTodo: string): void {
+    // in order to find the current id to know which to id, we use findIndex
+    let currentTodo = todos.findIndex((todo) => todo.id === id);
+    //take our new todo string and put it into our current index .text
+    todos[currentTodo].text = newTodo;
+  }
+
+  // function editTodo(id: string, newTodo: string): void {
+  //   let currentTodo = todos.findIndex((todo) => (todo.id = id));
+  //   todos[currentTodo].text = newTodo;
+  // }
 
   function clearCompleted() {
     todos = [];
@@ -62,12 +78,12 @@
     {#if todosAmount}
       <ul class="todo-list">
         {#each todos as todo (todo.id)}
-          <Todo {todo} {completeTodo} {removeTodo} />
+          <Todo {todo} {completeTodo} {removeTodo} {editTodo} />
         {/each}
       </ul>
 
       <div class="actions">
-        <span class="todo-count">0 left</span>
+        <TodosLeft {incompleteTodos} />
         <div class="filters">
           <button class="filter">All</button>
           <button class="filter">Active</button>
